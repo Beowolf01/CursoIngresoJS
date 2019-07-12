1804ccacf474f830f358be0e0edbcbee99bbf012
Ramiro Paredes <52410319+Beowolf01@users.noreply.github.com>
2019-07-11 22:51:16 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,72 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var numeroUno
+    var numeroDos
+    var sumar
+
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroUno = parseInt(numeroUno);
+
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroDos = parseInt(numeroDos);
+
+    suma = numeroUno + numeroDos;
+
+    alert("La suma es: " + suma);
+
 }
 
 function restar()
 {
-	
+    var numeroUno
+    var numeroDos
+    var resta
+
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroUno = parseInt(numeroUno);
+
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroDos = parseInt(numeroDos);
+
+    resta = numeroUno - numeroDos;
+
+    alert("La resta es: " + resta);
+
 }
 
 function multiplicar()
 { 
-	
+    var numeroUno
+    var numeroDos
+    var multiplicación
+
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroUno = parseInt(numeroUno);
+
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroDos = parseInt(numeroDos);
+
+    multiplicación = numeroUno * numeroDos;
+
+    alert("La multiplicación es: " + multiplicación);
+
 }
 
 function dividir()
 {
-	
+    var numeroUno
+    var numeroDos
+    var división
+
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroUno = parseInt(numeroUno);
+
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroDos = parseInt(numeroDos);
+
+    división = numeroUno / numeroDos;
+
+    alert("La división es: " + división);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,16 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldo
+    var resultado
+    var aumento
 
-	
+    sueldo = document.getElementById("sueldo").value;
+    sueldo = parseInt(sueldo);
+
+    resultado = document.getElementById("resultado");
+    resultado = parseInt(resultado)*1.1;
+
+    alert("El aumento es de: " + resultado);
+
 }
