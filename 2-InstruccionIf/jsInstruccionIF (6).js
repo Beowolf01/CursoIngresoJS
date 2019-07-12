552c0f8ccd73d95813ea02431326e7b9c3be964b
Ramiro Paredes <52410319+Beowolf01@users.noreply.github.com>
2019-07-12 12:10:56 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,21 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad
 
+    edad = document.getElementById("edad").value;
 
-
+    if(edad>=18)
+    {
+        alert("Usted es mayor de edad");
+    }
+    if(edad>=13 && edad<=17)
+    {
+        alert("Usted es adolescente");
+    }
+    else
+    {
+        alert("Usted es un niño");
+    }
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,18 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad
+    var estadocivil
 
-	
+    edad = document.getElementById("edad").value;
+    estadocivil = document.getElementById("estadoCivil").value;
+    
+    if(edad<18 && estadocivil != "Soltero")
+    
+    else
+    {
+        alert("Es soltero y no es menor");
+    }
 
 
 }//FIN DE LA FUNCIÓN
