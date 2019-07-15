08c32c3c128032edcec66e18ff5819daea8403d2
Beowolf01 <paredesramiro01@gmail.com>
2019-07-15 10:24:25 -0300

    En clase

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,22 +1,24 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	var numero
+	var numero;
 
 	numero = Math.floor(Math.random() * 10 ) + 1;
-
-	if(numero<4)
-	{
-		alert(numero + " Vamos, la proxima se puede");
-	}
 
 	if(numero>=9)
 	{
 		alert(numero + " Excelente");
 	}
-
 	else
 	{
-		alert(numero + " Aprobó");
+		if(numero>=4)
+		{
+			alert(numero + " Aprobó");
+		}
+		else
+		{
+			alert(numero + " Vamos, la proxima se puede");
+		}
 	}
+	
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -9,13 +9,17 @@ function mostrar()
     {
         alert("Usted es mayor de edad");
     }
-    if(edad<13)
-    {
-        alert("Usted es un niño");
-    }
-    else//Para no utilizar otro if o usar mas de 1, utilicé un else para la preposicion mas larga
-    {
-        alert("Usted es un adolescente");
-    }
+        else
+        {
+                if(edad<13)
+            {
+                alert("Usted es un niño");
+            }
+                 else//Para no utilizar otro if o usar mas de 1, utilicé un else para la preposicion mas larga
+                {
+                    alert("Usted es un adolescente");
+                }
+                    }
+    
 
 }//FIN DE LA FUNCIÓN
