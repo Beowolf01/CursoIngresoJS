ba435882ee7a1dfe96b1f8f10e7c75e8160b2342
Beowolf01 <paredesramiro01@gmail.com>
2019-07-19 12:09:48 -0300

    Clases

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,16 +1,44 @@
 function mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
+		var contador=0;
+		var numero;
+		alert(numero);
+		var numeroDos;
+		var suma;
+		alert(numero);
 
-	while(respuesta!="no")
-	{
+		numeroDos=12;
+		suma=numero*numeroDos;
+		alert(suma);
+		suma=3;//bien
+		suma="3";//bien
+		suma="lalala";//error
+
+		while((isNaN(suma))||(suma<0)|| (suma>10))
+		{
+			alert("error");
+			suma=prompt("Error, reingrese solo numeros");
+		}
+		alert("ingreso correcto " + suma);
+		
+		//if(isNaN(suma))
+		//{
+		//	alert("error");
+
+		//}else{
+		//	alert("bien");
+		//}
+
+		//declarar contadores y variables 
+	
+		//var respuesta="si";
+
+		//while(respuesta!="no")
+		//{
 		
 	
-	}
+		//}
 
 
 
